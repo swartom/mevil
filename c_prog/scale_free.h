895f484f26eb2c2874411aa3f04d77a79f1fda88
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-10-15 13:59:23 +0100

    Passthrough

--- c_prog/scale_free.h
+++ c_prog/scale_free.h
@@ -8,9 +8,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MAX 1000000
+#define MAX 250000000
 #define DIVISOR 2
-#define CONNECTIONS 1200
+#define CONNECTIONS 4
 #define THREADS 16  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
 #define LIMIT MAX/THREADS
 
