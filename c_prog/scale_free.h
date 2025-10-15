84faca386e94b47111f448892b22ded25dee8997
Thomas Swarbrick <swartom@localhost-live.a09.lan>
2025-10-15 16:51:41 +0100

    Passtrhough

--- c_prog/scale_free.h
+++ c_prog/scale_free.h
@@ -12,12 +12,12 @@
 #define MAX 250000000
 #define DIVISOR 2
 #define CONNECTIONS 4
-#define THREADS 32  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
+#define THREADS 64  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
 #define LIMIT MAX/THREADS
-#define REPETITIONS 100
+#define REPETITIONS 10
 #define PROCESSORS 32
 
-#define ALPHA 0.01
+#define ALPHA 0.5
 #define BETA 1.0
 
 #endif // SCALE_FREE_H_2
