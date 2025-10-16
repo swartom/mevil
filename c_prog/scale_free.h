ec6de5cd7274f352d90c452ae77aa2477a0f4662
swartom <tom@swarbrick.eu.org>
2025-10-16 21:12:15 +0100

    passthrough

--- c_prog/scale_free.h
+++ c_prog/scale_free.h
@@ -9,17 +9,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MAX 10000000
+#define MAX 15000000
 #define DIVISOR 2
-#define CONNECTIONS 30
-#define THREADS 256  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
+#define CONNECTIONS 24
+#define THREADS 128  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
 #define LIMIT MAX/THREADS
 #define REPETITIONS 5
-#define SECONDS_WAIT_BETWEEN_REPEATS 10
+#define SECONDS_WAIT_BETWEEN_REPEATS 4
 
 #define PROCESSORS 8
 
-#define ALPHA 0.01
+#define ALPHA 0.25
 #define BETA 1.0
 
 #endif // SCALE_FREE_H_2
