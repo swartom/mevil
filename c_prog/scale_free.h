ab8fd1658d9d82cd4d4a06571c0686d3619bbad4
swartom <tom@swarbrick.eu.org>
2025-10-16 21:06:18 +0100

    configuration for 41.36 with .5sdev

--- c_prog/scale_free.h
+++ c_prog/scale_free.h
@@ -12,14 +12,14 @@
 #define MAX 10000000
 #define DIVISOR 2
 #define CONNECTIONS 30
-#define THREADS 128  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
+#define THREADS 256  // (uint32_t)sysconf(_SC_NPROCESSORS_ONLN) * 8
 #define LIMIT MAX/THREADS
-#define REPETITIONS 1
-#define SECONDS_WAIT_BETWEEN_REPEATS 0
+#define REPETITIONS 5
+#define SECONDS_WAIT_BETWEEN_REPEATS 10
 
 #define PROCESSORS 8
 
-#define ALPHA 0.5
+#define ALPHA 0.01
 #define BETA 1.0
 
 #endif // SCALE_FREE_H_2
